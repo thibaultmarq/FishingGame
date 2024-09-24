df38d056ba50205a0a30359ead24ce999b09ba92
thibaultmarq <114607476+thibaultmarq@users.noreply.github.com>
2024-09-24 17:44:13 +0200

    Avancement mécanique de lancer
    définition des fonctions de puissance et choix d'angle, ainsi que de plusieurs objets

--- Totally Original Fishing Game/Assets/Scripts/FishingRod.cs
+++ Totally Original Fishing Game/Assets/Scripts/FishingRod.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class FishingRod : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Throw()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public Vector2 DefineThrowAngle()
     {
-        
+        Vector2 ThrowAngle = new Vector2(Gamepad.current.leftStick.x.ReadValue(), Gamepad.current.leftStick.y.ReadValue());
+        while (GameManager.Instance.GameState == GameState.ANGULARSELECTION)
+        {
+            ThrowAngle = new Vector2(Gamepad.current.leftStick.x.ReadValue(), Gamepad.current.leftStick.y.ReadValue());
+            UiManager.Instance.SetAngleArrow(ThrowAngle);
+        }
+
+
+        return ThrowAngle;
+    }
+
+    public float DefineThrowDistance() 
+    {
+        float currentValue = 0.0f;
+        int cursorDirection = 1;
+        while (GameManager.Instance.GameState == GameState.DISTANCESELECTION)
+        {
+            currentValue += cursorDirection * Time.deltaTime;
+            if (currentValue <= 0.0f || currentValue >= 1.0f) {
+                cursorDirection *= -1;
+            }
+            UiManager.Instance.SetDistanceSlider(currentValue);
+        }
+        return currentValue;
     }
 }
--- Totally Original Fishing Game/Assets/Scripts/GameManager.cs
+++ Totally Original Fishing Game/Assets/Scripts/GameManager.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject fishingSpotPrefab;
+    private GameObject fishingSpot;
+    private GameState gameState = GameState.MAIN;
+
+    private float fishingSpotCooldown;
+    [SerializeField] private float maxFishingSpotCooldown;
+
+    public GameState GameState { get { return gameState; } set { gameState = value; } }
+
+
+
+    private static GameManager instance = null;
+    public static GameManager Instance => instance;
+
+
+    private void Awake()
     {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        fishingSpotCooldown = maxFishingSpotCooldown;
+    }
+
+
+    private void Update()
+    {
+        fishingSpotCooldown -= Time.deltaTime;
+        if (fishingSpotCooldown < 0)
+        {
+            fishingSpotCooldown = maxFishingSpotCooldown;
+            DestroyOldSpot();
+            fishingSpot = GetNextSpot();
+        }
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private GameObject GetNextSpot()
     {
-        
+        return Instantiate(fishingSpotPrefab, new Vector3(Random.Range(-5,5),Random.Range(0,5),0), Quaternion.identity);
     }
+
+
+    public void DestroyOldSpot()
+    {
+        Destroy(fishingSpot);
+    }
+
 }
