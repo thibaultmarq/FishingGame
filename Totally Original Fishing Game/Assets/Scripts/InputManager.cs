25c2443c6709f2e392fa886d4cf03855547036e2
Xiran Kali <lucas.silver999@gmail.com>
2024-09-30 17:19:37 +0200

    Début implémentation Input Manager

--- Totally Original Fishing Game/Assets/Scripts/InputManager.cs
+++ Totally Original Fishing Game/Assets/Scripts/InputManager.cs
@@ -1,10 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+
+
+    void OnUp(InputValue value)
+    {
+        Debug.Log("truc");
+    }
+
+    void OnDown(InputValue value)
+    {
+        Debug.Log("bidule");
+    }
+
+
+    void OnLeft(InputValue value)
+    {
+
+    }
+
+    void OnRight(InputValue value)
+    {
+
+    }
+
+
+
+    void InputProcess(int value)
+    {
+
+    }
+
+
+    void OnTestAction(InputValue value)
+    {
+        Debug.Log(value.Get());
+    }
+
+
     void Start()
     {
         
